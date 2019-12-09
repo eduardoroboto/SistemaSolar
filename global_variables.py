334d831454c4fcb522231c42e4af8673b1f1840e
sred___ <eduardobarroso@edu.unifor.br>
2019-12-09 15:08:43 -0300

    Org
    Organização e mover as classes importantes para o Celestial.

--- global_variables.py
+++ global_variables.py
@@ -1,15 +1,11 @@
-from shapes import *
-from math import *
+from shapes import MAP
+from math import pi
 
-# Everything is in km
-# SUN_DIAMETER     = 109
 
 #Estrela
-# SUN_DIAMETER     = 109.17
 SUN_DIAMETER = 10
 
 #Planetas
-
 EARTH_DIAMETER   = 1
 MERCURY_DIAMETER = 0.383
 VENUS_DIAMETER   = 0.949
@@ -40,7 +36,7 @@ semimajor_axis_Jupiter = 5.2029
 semimajor_axis_Saturn = 9.5370
 semimajor_axis_Neptune = 19.1890
 semimajor_axis_Uranus = 30.0699
-semimajor_axis_Pluto = 39.4821
+# semimajor_axis_Pluto = 39.4821
 
 Mercury_orbital_period = 0.48
 Mercury_eccentricity = 0.21
@@ -57,7 +53,7 @@ Uranus_eccentricity = 0.05
 Neptune_orbital_period = 164.8
 Neptune_eccentricity = 0.01
 Pluto_orbital_period = 248.0
-Pluto_eccentricity = 0.25
+# Pluto_eccentricity = 0.25
 
 
 
@@ -90,7 +86,7 @@ JAPETO_DIAMETER    = 0.115288
 # uranus
 UMBRIEL_DIAMETER   = 0.091775
 TITANIA_DIAMETER   = 0.123748
-OBERON_DIAMETER    = 0.119510
+OBERON_DIAMMercury_orbital_periodETER    = 0.119510
 # neptune
 TRITAO_DIAMETER    = 0.212431
 PROTEU_DIAMETER    = 0.032962
@@ -126,111 +122,14 @@ NEREIDA_DISTANCE = 0.03685
 #terra
 MOON_VELOCITY=   0.0748
 
-# #marte
-# FOBOS_VELOCITY = 0
-# DEIMOS_VELOCITY = 0
-# # jupiter
-# EUROPA_VELOCITY = 0
-# GANIMEDES_VELOCITY = 0
-# IO_VELOCITY = 0
-# CALISTO_VELOCITY = 0
-# # saturn
-# REIA_VELOCITY = 0
-# TITA_VELOCITY = 0
-# JAPETO_VELOCITY = 0
-# # uranus
-# UMBRIEL_VELOCITY = 0
-# TITANIA_VELOCITY = 0
-# OBERON_VELOCITY = 0
-# # neptune
-# TRITAO_VELOCITY = 0
-# PROTEU_VELOCITY = 0
-# NEREIDA_VELOCITY = 0
-
-
-#terra
-#marte
-# MERCURY_COLOR=
-# VENUS_COLOR=
-# EARTH_COLOR=
-# MOON_COLOR=
-# MARS_COLOR=
-# JUPITER_COLOR=
-# SATURN_COLOR=
-# URANUS_COLOR=
-# NEPTUNE_COLOR=
-
-
-# 0.241 	0.615 	1 	1.88 	11.9 	29.4 	83.7 	163.7 	247.9
-#
-# 5906376272
-# 9999999999
 def RADIUS_MAP(DIAMETER):
+    """ Convert DIAMETER TO RADIUS"""
     return (DIAMETER/2)
-    # return MAP(DIAMETER,PLUTO_DIAMETER,JUPITER_DIAMETER,0,5)
 
 def DISTANCE_MAP(distance):
+    """ Scale distance AU to a number"""
     return distance*12
-    # return MAP(distance,0,PLUTO_DISTANCE,0,20)
-
-# 147.1
-
 
 def VELOCITY_MAP(velocity):
+    """Scale VELOCITY to a round number """
     return round(MAP(round(velocity*100*pi),0,1000,1,360*5))
-
-
-def keplerIII_period_to_semimajor_axis(orbital_period):
-    #########################################################
-    # Units: orbital period [yr], separation [au]           #
-    #########################################################
-    semimajor_axis_cubed = orbital_period ** 2
-    semimajor_axis = semimajor_axis_cubed ** (1. / 3.)
-
-    return semimajor_axis
-
-
-def make_kepler_orbit(eccentricity, orbital_period,nStep):
-    ##########################################################
-    # Units: orbital period [years]                          #
-    # returns: 500 true anomaly values throughout the orbit  #
-    ##########################################################
-    # nStep = 500
-    # tRange = np.linspace(0.0, orbital_period, nStep)
-    lower = 0.0
-    upper = orbital_period
-    length = nStep
-    # [lower + x * (upper - lower) / (length - 1) for x in range(length)]
-    # [lower + x * (upper - lower) / length for x in range(length)]
-    tRange = [lower + x * (upper - lower) / (length - 1) for x in range(length)]
-
-
-    theta = []
-    for time in tRange:
-        PsiDiff = 1.0
-        M = 2 * pi * time / orbital_period
-        PsiOld = M
-        theta0old = 180.0
-        while PsiDiff > 1e-10:
-            PsiNew = M + eccentricity * sin(PsiOld)
-            PsiDiff = PsiNew - PsiOld
-            PsiOld = PsiNew
-        theta0 = 2 * atan(((1 + eccentricity) / (1 - eccentricity)) ** (0.5) * tan(PsiOld / 2.))
-        theta.append(theta0)
-    return theta
-
-# list1 = make_kepler_orbit(eccentricity,orbital_period)
-
-def orbit(radius,semimajor_axis, eccentricity, true_anomaly_list):
-    ##############################################
-    # Units: separation [au] #
-    ##############################################
-    x_orbit = []
-    y_orbit = []
-    # define the shape equation
-    for true_anomaly in true_anomaly_list:
-        r_orbit = semimajor_axis * (1 - eccentricity ** 2) / (1 + eccentricity * cos(true_anomaly))
-        x_orbit.append(r_orbit * cos(true_anomaly))
-        y_orbit.append(r_orbit * sin(true_anomaly))
-
-    return x_orbit, y_orbit
